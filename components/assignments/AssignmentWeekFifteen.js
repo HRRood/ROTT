677fdd857e5f826128ce015d1de29aa0b4d63e05
Markus <mdvanleeuwen@gmail.com>
2022-12-13 02:01:57 +0100

    styled results

--- components/assignments/AssignmentWeekFifteen.js
+++ components/assignments/AssignmentWeekFifteen.js
@@ -27,7 +27,9 @@ export const getItemStyle = (isDragging, draggableStyle) => ({
   borderRadius: "10px",
 
   // change background colour if dragging
-  background: isDragging ? "var(--adsai-medium-blue)" : "var(--adsai-light-blue)",
+  background: isDragging
+    ? "var(--adsai-medium-blue)"
+    : "var(--adsai-light-blue)",
   border: "1px solid var(--adsai-medium-blue)",
   color: "black",
   boxShadow: isDragging ? "rgba(0, 0, 0, 0.5) 0px 3px 12px" : "none",
@@ -53,7 +55,7 @@ const getListStyle = (isDraggingOver) => ({
   border: "1px solid rgba(0, 0, 0, 0.1)",
 });
 export function AssignmentWeekFifteen() {
-  const [draggableGroupItems, setDraggableGroupItems] = useState([[], [], [], [], [], [], []]);
+  const [draggableGroupItems, setDraggableGroupItems] = useState(EXAMPLEDATA);
   const [hasSubmitted, setHasSubmitted] = useState(false);
   const [totalHoursFilled, setTotalHoursFilled] = useState(0);
   const [pointsLogged, setPointsLogged] = useState([]);
@@ -74,35 +76,62 @@ export function AssignmentWeekFifteen() {
 
         const pointsLoggedTemp = [];
         groupedItems.forEach((group) => {
-          const activityData = activities.find((a) => a.value === group.activityName);
+          const activityData = activities.find(
+            (a) => a.value === group.activityName
+          );
 
           if (activityData.type === "neutral") {
-            pointsLoggedTemp.push({ activityName: activityData.label, message: "Geen punten gegeven, neutrale activiteit" });
+            pointsLoggedTemp.push({
+              activityName: activityData.label,
+              score: 0,
+              message: "Geen punten gegeven, neutrale activiteit",
+            });
             return;
           }
 
           if (activityData.type === "negative") {
             if (group.time > activityData.max) {
-              pointsLoggedTemp.push({ activityName: activityData.label, message: "-3 punten, te veel tijd besteed aan de activiteit" });
+              pointsLoggedTemp.push({
+                activityName: activityData.label,
+                score: -3,
+                message: "Te veel tijd besteed aan de activiteit",
+              });
               return;
             }
 
-            pointsLoggedTemp.push({ activityName: activityData.label, message: "Geen punten gegeven, de maximale tijd niet overschreden" });
+            pointsLoggedTemp.push({
+              activityName: activityData.label,
+              score: 0,
+              message:
+                "Geen punten gegeven, de maximale tijd niet overschreden",
+            });
             return;
           }
 
           if (activityData.type === "positive") {
             if (group.time > activityData.max) {
-              pointsLoggedTemp.push({ activityName: activityData.label, message: "-1 punt, te veel tijd besteed aan de activiteit" });
+              pointsLoggedTemp.push({
+                activityName: activityData.label,
+                score: -1,
+                message: "Te veel tijd besteed aan de activiteit",
+              });
               return;
             }
 
             if (group.time < activityData.min) {
-              pointsLoggedTemp.push({ activityName: activityData.label, message: "-2 punten, te weinig tijd besteed aan activiteit" });
+              pointsLoggedTemp.push({
+                activityName: activityData.label,
+                score: -2,
+                message: "Te weinig tijd besteed aan activiteit",
+              });
               return;
             }
 
-            pointsLoggedTemp.push({ activityName: activityData.label, message: "+2 punten, het valt binnen de tijdslimiet" });
+            pointsLoggedTemp.push({
+              activityName: activityData.label,
+              score: 2,
+              message: "Valt binnen de tijdslimiet",
+            });
             return;
           }
         });
@@ -114,7 +143,10 @@ export function AssignmentWeekFifteen() {
 
   useEffect(() => {
     const totalHoursFilled = draggableGroupItems.reduce((acc, cur) => {
-      const totalHours = cur.reduce((acc, cur) => (cur ? acc + parseFloat(cur.time || 0) : 0), 0);
+      const totalHours = cur.reduce(
+        (acc, cur) => (cur ? acc + parseFloat(cur.time || 0) : 0),
+        0
+      );
       return acc + totalHours;
     }, 0);
     setTotalHoursFilled(totalHoursFilled);
@@ -154,12 +186,21 @@ export function AssignmentWeekFifteen() {
     const dInd = +destination.droppableId;
 
     if (sInd === dInd) {
-      const items = reorder(draggableGroupItems[sInd], source.index, destination.index);
+      const items = reorder(
+        draggableGroupItems[sInd],
+        source.index,
+        destination.index
+      );
       const newState = [...draggableGroupItems];
       newState[sInd] = items;
       setDraggableGroupItems(newState);
     } else {
-      const result = move(draggableGroupItems[sInd], draggableGroupItems[dInd], source, destination);
+      const result = move(
+        draggableGroupItems[sInd],
+        draggableGroupItems[dInd],
+        source,
+        destination
+      );
       const newState = [...draggableGroupItems];
       newState[sInd] = result[sInd] || [];
       newState[dInd] = result[dInd] || [];
@@ -200,11 +241,16 @@ export function AssignmentWeekFifteen() {
     return arr.reduce((acc, cur) => {
       cur.forEach((item) => {
         if (item && item.activityName) {
-          const activity = acc.find((a) => a.activityName === item.activityName);
+          const activity = acc.find(
+            (a) => a.activityName === item.activityName
+          );
           if (activity) {
             activity.time += parseFloat(item.time);
           } else {
-            acc.push({ activityName: item.activityName, time: parseFloat(item.time) });
+            acc.push({
+              activityName: item.activityName,
+              time: parseFloat(item.time),
+            });
           }
         }
       });
@@ -223,32 +269,56 @@ export function AssignmentWeekFifteen() {
       if (!activityData) return acc;
 
       if (activityData.type === "neutral") {
-        pointsLoggedTemp.push({ activityName: activityData.label, message: "Geen punten gegeven, neutrale activiteit" });
+        pointsLoggedTemp.push({
+          activityName: activityData.label,
+          score: 0,
+          message: "Geen punten gegeven, neutrale activiteit",
+        });
         return acc;
       }
 
       if (activityData.type === "negative") {
         if (cur.time > activityData.max) {
-          pointsLoggedTemp.push({ activityName: activityData.label, message: "-3 punten, te veel tijd besteed aan de activiteit" });
+          pointsLoggedTemp.push({
+            activityName: activityData.label,
+            score: -3,
+            message: "Te veel tijd besteed aan de activiteit",
+          });
           return acc - 3;
         }
 
-        pointsLoggedTemp.push({ activityName: activityData.label, message: "Geen punten gegeven, de maximale tijd niet overschreden" });
+        pointsLoggedTemp.push({
+          activityName: activityData.label,
+          score: 0,
+          message: "Geen punten gegeven, de maximale tijd niet overschreden",
+        });
         return acc;
       }
 
       if (activityData.type === "positive") {
         if (cur.time > activityData.max) {
-          pointsLoggedTemp.push({ activityName: activityData.label, message: "-1 punt, te veel tijd besteed aan de activiteit" });
+          pointsLoggedTemp.push({
+            activityName: activityData.label,
+            score: -1,
+            message: "Te veel tijd besteed aan de activiteit",
+          });
           return acc - 1;
         }
 
         if (cur.time < activityData.min) {
-          pointsLoggedTemp.push({ activityName: activityData.label, message: "-2 punten, te weinig tijd besteed aan activiteit" });
+          pointsLoggedTemp.push({
+            activityName: activityData.label,
+            score: -2,
+            message: "Te weinig tijd besteed aan activiteit",
+          });
           return acc - 2;
         }
 
-        pointsLoggedTemp.push({ activityName: activityData.label, message: "+2 punten, het valt binnen de tijdslimiet" });
+        pointsLoggedTemp.push({
+          activityName: activityData.label,
+          score: 2,
+          message: "Valt binnen de tijdslimiet",
+        });
         return acc + 2;
       }
     }, STARTPOINTS);
@@ -269,47 +339,111 @@ export function AssignmentWeekFifteen() {
       console.log("res", res);
       setHasSubmitted(true);
     });
+  };
 
-    console.log("rororo TOTALPOINTS", groupedActivities, pointsLoggedTemp, totalPoints);
+  const setResultDivClass = (numPoints) => {
+    if (numPoints >= STARTPOINTS) {
+      return `${styles.alert} ${styles.alert_success}`;
+    } else {
+      return `${styles.alert} ${styles.alert_fail}`;
+    }
+  };
+
+  const setResultTableDivClass = (numPoints) => {
+    if (numPoints >= STARTPOINTS) {
+      return `${styles.result_table_container} ${styles.result_table_container_success}`;
+    } else {
+      return `${styles.result_table_container} ${styles.result_table_container_fail}`;
+    }
+  };
+
+  const setTableClass = (numPoints) => {
+    if (numPoints >= STARTPOINTS) {
+      return `${styles.result_table} ${styles.result_table_success}`;
+    } else {
+      return `${styles.result_table} ${styles.result_table_fail}`;
+    }
+  };
+
+  const showResultsTable = (hasSubmitted) => {
+    if (hasSubmitted === true) {
+      return `${styles.result_container}`;
+    } else {
+      return `${styles.result_container_hidden}`;
+    }
   };
 
   return (
     <>
-      {pointsLogged.length > 0 && (
+      <div className={showResultsTable(hasSubmitted)}>
         <div>
-          <table>
-            <tr>
-              <th>Activiteit</th>
-              <th></th>
-            </tr>
-            {pointsLogged.map((item, index) => (
-              <tr key={index}>
-                <td>{item.activityName}</td>
-                <td>{item.message}</td>
-              </tr>
-            ))}
-          </table>
-        </div>
-      )}
-      {hasSubmitted && (
-        <div className={styles.alert}>
-          <p>
-            Je hebt <strong>{totalPointsAssignment}</strong> punten gekregen voor de opdracht. Dit is{" "}
-            <strong>{totalPointsAssignment >= STARTPOINTS ? "voldoende" : "onvoldoende"}</strong>.
-          </p>
-          {totalPointsAssignment < STARTPOINTS && (
-            <p>
-              Je planning kan beter! Je komt <strong>{STARTPOINTS - totalPointsAssignment}</strong> punten tekort. Zie je verbeterpunten?
-            </p>
+          {hasSubmitted && (
+            <div className={setResultDivClass(totalPointsAssignment)}>
+              <span>
+                Je hebt <strong>{totalPointsAssignment}</strong> punten gekregen
+                voor de opdracht. Dit is{" "}
+                <strong>
+                  {totalPointsAssignment >= STARTPOINTS
+                    ? "voldoende"
+                    : "onvoldoende"}
+                </strong>
+                .&nbsp;
+              </span>
+
+              {totalPointsAssignment < STARTPOINTS && (
+                <span>
+                  Je planning kan beter. Je komt{" "}
+                  <strong>{STARTPOINTS - totalPointsAssignment}</strong> punten
+                  tekort. Zie je verbeterpunten?
+                </span>
+              )}
+              {totalPointsAssignment >= STARTPOINTS && (
+                <span>
+                  Je planning is goed! Je hebt de juiste hoeveelheid tijd
+                  besteed aan de activiteiten.
+                </span>
+              )}
+            </div>
           )}
-          {totalPointsAssignment >= STARTPOINTS && <p>Je planning is goed! Je hebt de juiste hoeveelheid tijd besteed aan de activiteiten.</p>}
         </div>
-      )}
-      <div style={{ display: "flex", gap: "10px", flexWrap: "wrap", margin: "20px 0" }}>
+        <div className={setResultTableDivClass(totalPointsAssignment)}>
+          {pointsLogged.length > 0 && (
+            <div>
+              <table className={setTableClass(totalPointsAssignment)}>
+                <tr>
+                  <th>Activiteit</th>
+                  <th>Score</th>
+                  <th>Toelichting</th>
+                </tr>
+                {pointsLogged.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item.activityName}</td>
+                    <td>{item.score}</td>
+                    <td>{item.message}</td>
+                  </tr>
+                ))}
+              </table>
+            </div>
+          )}
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          gap: "10px",
+          flexWrap: "wrap",
+          margin: "20px 0",
+        }}
+      >
         <DragDropContext onDragEnd={onDragEnd}>
           {draggableGroupItems.map((draggableGroupItem, index) => {
-            const hasUnfilledHour = draggableGroupItem.some((item) => !item || !item.time || !item.activityName);
-            const totalHoursFilled = draggableGroupItem.reduce((acc, cur) => (cur ? acc + parseFloat(cur.time || 0) : 0), 0);
+            const hasUnfilledHour = draggableGroupItem.some(
+              (item) => !item || !item.time || !item.activityName
+            );
+            const totalHoursFilled = draggableGroupItem.reduce(
+              (acc, cur) => (cur ? acc + parseFloat(cur.time || 0) : 0),
+              0
+            );
 
             const hoursLeft = 10 - totalHoursFilled;
             return (
@@ -324,20 +458,30 @@ export function AssignmentWeekFifteen() {
                   >
                     <div style={{ margin: "5px" }}>
                       <h3 style={{ margin: 0, padding: 0 }}>{days[index]}</h3>
-                      <p style={{ margin: 0, padding: 0 }}>{hoursLeft} uur in te plannen</p>
+                      <p style={{ margin: 0, padding: 0 }}>
+                        {hoursLeft} uur in te plannen
+                      </p>
                     </div>
                     <div style={{ flex: "1" }}>
                       {draggableGroupItem.map((item, itemIndex) => {
                         return (
-                          <Draggable key={item.id} draggableId={item.id} index={itemIndex}>
+                          <Draggable
+                            key={item.id}
+                            draggableId={item.id}
+                            index={itemIndex}
+                          >
                             {(provided, snapshot) => (
                               <DragItem
                                 item={item}
                                 hoursLeft={hoursLeft}
                                 provided={provided}
                                 snapshot={snapshot}
-                                setItemData={(data) => setItemInGroup(index, itemIndex, data)}
-                                removeFromList={() => removeFromList(index, itemIndex)}
+                                setItemData={(data) =>
+                                  setItemInGroup(index, itemIndex, data)
+                                }
+                                removeFromList={() =>
+                                  removeFromList(index, itemIndex)
+                                }
                                 hasSubmitted={hasSubmitted}
                               />
                             )}
@@ -346,7 +490,11 @@ export function AssignmentWeekFifteen() {
                       })}
                     </div>
                     {provided.placeholder}
-                    {hoursLeft > 0 && !hasUnfilledHour && <button onClick={() => addItemToGivenList(index)}>Add</button>}
+                    {hoursLeft > 0 && !hasUnfilledHour && (
+                      <button onClick={() => addItemToGivenList(index)}>
+                        Add
+                      </button>
+                    )}
                   </div>
                 )}
               </Droppable>
--- pages/assignments/assignmentweekfifteen.js
+++ pages/assignments/assignmentweekfifteen.js
@@ -40,28 +40,49 @@ export default function AssignmentWeekFifteenPage({ userData }) {
           <Row>
             <Column width={100} smWidth={100} lgWidth={70}>
               <p>
-                Voor school ben je druk bezig met een groot project. Je zit met je groep in de afrondende fase, en maandag over een week is de deadline.
-                Aankomende week heb je nog de tijd om alles tot een mooi resultaat te brengen. Naar verwachting zal dit je nog 20 uur aan huiswerk kosten.
+                Voor school ben je druk bezig met een groot project. Je zit met
+                je groep in de afrondende fase, en maandag over een week is de
+                deadline. Aankomende week heb je nog de tijd om alles tot een
+                mooi resultaat te brengen. Naar verwachting zal dit je nog 20
+                uur aan huiswerk kosten.
               </p>
               <p>
-                Behalve het schoolwerk heb je natuurlijk nog meer bezigheden gedurende de week. Natuurlijk moet je overdag naar school. Verder kun je denken aan
-                huishoudelijke taken, boodschappen doen, de hond uitlaten als je die hebt, met vrienden afspreken, sporten, enzovoort. In deze opdracht wordt je
-                gevraagd om je week zo in te delen, dat je de deadline haalt.
+                Behalve het schoolwerk heb je natuurlijk nog meer bezigheden
+                gedurende de week. Natuurlijk moet je overdag naar school.
+                Verder kun je denken aan huishoudelijke taken, boodschappen
+                doen, de hond uitlaten als je die hebt, met vrienden afspreken,
+                sporten, enzovoort. In deze opdracht wordt je gevraagd om je
+                week zo in te delen, dat je de deadline haalt.
               </p>
               <p>
-                In onderstaand schema kun je activiteiten plaatsen. Je hoeft de standaard activiteiten zoals slapen, douchen en eten niet in te delen. In deze
-                oefening zijn die iedere dag hetzelfde, dus daar hoef je geen aandacht aan te besteden. Er blijft 12 uur per dag over om in te delen. Als je een
-                activiteit plaatst, kun je de tijd instellen die je eraan denkt te besteden. Ook kun je de activiteiten in een voor jou handige volgorde zetten.
+                In onderstaand schema kun je activiteiten plaatsen. Je hoeft de
+                standaard activiteiten zoals slapen, douchen en eten niet in te
+                delen. In deze oefening zijn die iedere dag hetzelfde, dus daar
+                hoef je geen aandacht aan te besteden. Er blijft 10 uur per dag
+                over om in te delen. Als je een activiteit plaatst, kun je de
+                tijd instellen die je eraan denkt te besteden. Ook kun je de
+                activiteiten in een voor jou handige volgorde zetten. Je begint
+                met 10 punten. Sommige activiteiten leveren punten op, tenzij je
+                er te veel uren in steekt. Andere activiteiten leveren geen
+                punten op, en weer andere leveren sowieso een puntenaftrek op.
+                Als je na afloop 10 of meer punten hebt gehaald, heb je de
+                oefening met succes afgerond.
               </p>
               <ol>
-                <li>Vul eerst je dagelijkse verplichtingen in. Denk aan school, de reis er naartoe, werk, enzovoort. Wees zo realistisch mogelijk.</li>
                 <li>
-                  Vul vervolgens het schema aan met de andere activiteiten die je deze week nog graag wilt doen. Ga je uit? Wil je nog sporten? Of gewoon een
-                  paar uur niks doen?
+                  Vul eerst je dagelijkse verplichtingen in. Denk aan school, de
+                  reis er naartoe, werk, enzovoort. Wees zo realistisch
+                  mogelijk.
                 </li>
                 <li>
-                  Nadat je de hele week gevuld hebt, zal blijken of je voldoende tijd voor opdracht hebt gereserveerd. Ook zul je zien hoeveel tijd andere
-                  activiteiten in kunnen nemen in de week.
+                  Vul vervolgens het schema aan met de andere activiteiten die
+                  je deze week nog graag wilt doen. Ga je uit? Wil je nog
+                  sporten? Of gewoon een paar uur niks doen?
+                </li>
+                <li>
+                  Nadat je de hele week gevuld hebt, zal blijken of je voldoende
+                  tijd voor opdracht hebt gereserveerd. Ook zul je zien hoeveel
+                  tijd andere activiteiten in kunnen nemen in de week.
                 </li>
               </ol>
             </Column>
@@ -84,14 +105,22 @@ function FillChapterList() {
   for (let i = 0; i < 7; i++) {
     const assignment = data.AssignmentList[i];
     const chapterFoldoutKey = `chapterFoldoutKey_${assignment.Id}`;
-    const chapter = <AssignmentChapterFoldOut key={chapterFoldoutKey} assignment={assignment}></AssignmentChapterFoldOut>;
+    const chapter = (
+      <AssignmentChapterFoldOut
+        key={chapterFoldoutKey}
+        assignment={assignment}
+      ></AssignmentChapterFoldOut>
+    );
     chapters.push(chapter);
   }
 
   return chapters;
 }
 
-export const getServerSideProps = withIronSessionSsr(async function ({ req, res }) {
+export const getServerSideProps = withIronSessionSsr(async function ({
+  req,
+  res,
+}) {
   if (!isUserLoggedIn(req)) {
     res.setHeader("location", "/login");
     res.statusCode = 302;
@@ -117,4 +146,5 @@ export const getServerSideProps = withIronSessionSsr(async function ({ req, res 
   return {
     props: { userData: { ...req.session.user, ...userMapped } },
   };
-}, sessionOptions);
+},
+sessionOptions);
